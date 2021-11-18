9201990203d9b9b209b520b7c421d951470c1480
wangl <wangl@hopson.com.cn>
2021-11-18 12:43:19 +0800

    add key store

--- docker-run-compose.sh
+++ docker-run-compose.sh
@@ -3,5 +3,9 @@
 docker stop cas_compose > /dev/null 2>&1
 docker rm cas_compose > /dev/null 2>&1
 image_tag=(`cat gradle.properties | grep "cas.version" | cut -d= -f2`)
-docker run -d -p 8080:8080 -p 8443:8443 --name="cas_compose" cas-overlay-template_cas:latest
+
+docker run -d -p 8080:8080 -p 8443:8443 --name="cas_compose" \
+-v /etc/cas/thekeystore:/etc/cas/thekeystore \
+cas-overlay-template_cas:latest
+
 docker logs -f cas_compose
